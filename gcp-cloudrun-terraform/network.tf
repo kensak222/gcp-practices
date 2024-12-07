e90046b82154b8fff1712eccca2dc83d7da88b6d
佐々木絢悟 <p4k54z2o@s.okayama-u.ac.jp>
2024-12-07 14:46:59 +0900

    add: VPC Access Connectorを作成したのでコミット

--- gcp-cloudrun-terraform/network.tf
+++ gcp-cloudrun-terraform/network.tf
@@ -1,3 +1,11 @@
 resource "google_compute_network" "vpc_network" {
   name = "main-network"
 }
+
+# CloudRunからVPC内にアクセスするために必要なVPC Access Connectorを作成
+# VPC Access Connectorは、VPC Network内の10.20.0.0/28というIPアドレス範囲をConnectorに割り当てる
+resource "google_vpc_access_connector" "connector" {
+  name          = "vpc-con"
+  ip_cidr_range = "10.20.0.0/28"
+  network       = google_compute_network.vpc_network.id
+}
